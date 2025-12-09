ac510c150e3e9c4cf714560e594303d0e305feaa
NikolaiYakimov <niki024204@gmail.com>
2025-12-09 13:00:29 +0200

    Almost complete repositories , and implement 1 new entity: Role.java and also modify Transaction.java and User.java

---
+++ src/main/java/FinanceTracker/repository/CurrencyRepository.java
@@ -0,0 +1,15 @@
+package FinanceTracker.repository;
+
+import FinanceTracker.entity.Currency;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CurrencyRepository extends JpaRepository<Currency,Long> {
+
+//Search by currency code
+    Optional<Currency> findByCode(String code);
+
+    boolean existByCode(String code);
+}
+
