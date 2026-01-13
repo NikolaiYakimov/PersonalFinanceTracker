7dd35a2f24b51a4dcfd270041e4ad414aafd0da0
Nikolai Yakimov <niki024204@gmai.com>
2026-01-13 16:48:32 +0200

    fix: remove bug in Category.java entity

--- src/main/java/FinanceTracker/entity/Category.java
+++ src/main/java/FinanceTracker/entity/Category.java
@@ -17,7 +17,7 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String name;
 
     @Enumerated(EnumType.STRING)
